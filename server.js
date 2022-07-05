c92655be12d9b9cafd1cfdf33168bd366906bcac
whatdoineed2do/Ray <whatdoineed2do@nospam.gmail.com>
2022-07-05 12:09:02 +0100

    [js] add ip dep

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const { getProducts, getProduct, createProduct, updateProduct, deleteProduct } = require('./controllers/productController')
+var ip = require("ip")
 
 const server = http.createServer((req, res) => {
 
@@ -16,6 +17,7 @@ const server = http.createServer((req, res) => {
     else if (req.url === "/api/health" && req.method === "GET") {
         res.writeHead(200, { "Content-Type": "application/json" });
         const stat = {
+	    ip: ip.address(),
             uptime: process.uptime(),
             timestamp: Date.now(),
         };
@@ -40,7 +42,7 @@ const server = http.createServer((req, res) => {
     }
 })
 
-const PORT =  process.env.PORT || 5000
+const PORT =  process.env.PORT || 8080
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
